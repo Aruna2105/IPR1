965c0c086579c9994cb04ecf66b6a4c15f670f2e
Aruna2105 <62954160+Aruna2105@users.noreply.github.com>
2020-03-31 22:47:55 +0530

    Add files via upload

---
+++ ipr_python.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import math as ma
+import matplotlib.pyplot as plt
+from sklearn.decomposition import FactorAnalysis
+from sklearn.decomposition import PCA
+from sklearn.metrics import jaccard_similarity_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import roc_curve,auc
+from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from math import sqrt
+from sklearn import tree
+import scipy.stats as ss
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix,classification_report
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[3]:
+
+
+import pandas as pd
+import stasmodels.api as sm
+from sklearn import linear_model
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+# In[ ]:
+
+
+
+
